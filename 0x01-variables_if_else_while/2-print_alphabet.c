ca270dfaeede7be280c9650e20e8ac11d2ad5216
Juansu01 <thejuank0@hotmail.com>
2021-02-04 22:14:36 +0000

    Added new c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print the whole alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	char y;
+
+
+	for (y = 'a'; y <= 'z'; y++)
+	{
+		putchar(y);
+
+
+
+	}
+	putchar(10);
+	return (0);
+}
