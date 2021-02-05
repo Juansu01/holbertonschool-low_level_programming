d7881fb65a94a75fb4f9213567cf73d598df631b
Juansu01 <thejuank0@hotmail.com>
2021-02-05 13:22:53 +0000

    Added new c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print all numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+
+
+
+	}
+	putchar('\n');
+	return (0);
+}
