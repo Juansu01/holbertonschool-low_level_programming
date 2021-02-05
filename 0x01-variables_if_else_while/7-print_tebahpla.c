dfc7befedf924da64706d2984e92f5519a8d1ac0
Juansu01 <thejuank0@hotmail.com>
2021-02-05 16:39:55 +0000

    Added new c file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print reverse alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	char y;
+
+
+	for (y = 'z'; y >= 'a'; y--)
+	{
+		putchar(y);
+
+
+
+	}
+	putchar(10);
+	return (0);
+}
