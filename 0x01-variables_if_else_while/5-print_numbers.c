38f525236f74837cbd088b5fd7779096b63177c2
Juansu01 <thejuank0@hotmail.com>
2021-02-05 11:20:17 +0000

    Added a new c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print all numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+
+
+
+	}
+	return (0);
+}
