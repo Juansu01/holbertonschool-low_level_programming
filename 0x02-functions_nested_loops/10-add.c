ad03c02aa169054682bf8e552ce1aa11579a48f4
Juansu01 <thejuank0@hotmail.com>
2021-02-09 17:22:58 +0000

    Added new c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * add - Add numbers.
+ * @a: First number
+ * @b: Second number
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	printf("%d\n", sum);
+	return 0;
+}
