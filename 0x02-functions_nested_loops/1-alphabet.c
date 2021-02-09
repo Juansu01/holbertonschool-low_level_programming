c803b0bc4d05b66ad8609a159e5cd4cef337193f
Juansu01 <thejuank0@hotmail.com>
2021-02-09 09:23:35 +0000

    Added new c file and modified header

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+
+#include "holberton.h"
+
+
+/**
+ * main - print the alphabet
+ *
+ * Return: Always 0.
+ *
+ */
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+
+	}
+
+	_putchar(10);
+}
