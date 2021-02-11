edd1538f318fb1738d0b9024e3b4f64e8374127b
Juansu01 <thejuank0@hotmail.com>
2021-02-11 18:14:06 +0000

    Added new c file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - print almost every number.
+ *
+ * Return: Always 0.
+ *
+ *
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		if (n != '2' && n != '4')
+	{
+		_putchar(n);
+	}
+
+	_putchar('\n');
+}
