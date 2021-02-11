ef594d369e9290fb2f9ec4b20865e3cf0c413599
Juansu01 <thejuank0@hotmail.com>
2021-02-11 20:30:31 +0000

    Added new c file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 10 lines of numbers from 0 to 14.
+ *
+ *
+ * Return: Always 0.
+ *
+ *
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	int a;
+	int fd;
+	int ld;
+
+	for (i = 0; i < 11; i++)
+	{
+
+		for (a = 0; a < 15; a++)
+		{
+
+
+			if (a > 9)
+			{
+				fd = (a / 10);
+				putchar(fd + '0');
+			}
+			ld = (a % 10);
+			putchar(ld + '0');
+
+
+		}
+
+		putchar('\n');
+
+	}
+
+}
