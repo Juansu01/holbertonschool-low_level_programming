62b895e215957157cef0df1ce9808f8d34b5ca49
Juansu01 <thejuank0@hotmail.com>
2021-02-16 19:35:01 +0000

    Added new c file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strlen - return length of a string.
+ *
+ *@s: String.
+ *
+ *
+ *
+ * Return: int.
+ */
+
+int _strlen(char *s)
+{
+	int a;
+	/*Repeat loop until you meet the null char*/
+	for (a = 0; s[a] != '\0'; a++)
+	{
+	}
+	return (a);
+}
