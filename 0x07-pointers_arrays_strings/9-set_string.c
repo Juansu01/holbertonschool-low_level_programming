35b7c2b6a3f1b5730488c919d5608fa8b3f501ea
Juansu01 <thejuank0@hotmail.com>
2021-02-22 23:44:41 +0000

    Added new c file

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * set_string - set value of pointer to a char.
+ *
+ * @s: Pointer that will be set to char.
+ * @to: Char pointer.
+ *
+ *
+ * Return: Doesn't return'.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
