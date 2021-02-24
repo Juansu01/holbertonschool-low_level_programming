5cfe8d0ee2b6707a6e1b7584a9cd1a6e82578533
Juansu01 <thejuank0@hotmail.com>
2021-02-24 22:43:54 +0000

    Added a new function that powers a value

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+* _pow_recursion - Return value of x raised to the pwr of y.
+*
+* @x: Value
+*
+* @y: Power
+*
+* Return: Number raised to the power of y.
+*
+*
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
+
