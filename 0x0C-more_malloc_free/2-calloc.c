27b1e3205bbad812cdb7fdbcc1f610b89aa65d4b
Juansu01 <thejuank0@hotmail.com>
2021-03-04 21:30:42 +0000

    Changed pointer, initialized to 0 and changed to unsigned int

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,17 +14,22 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	char *p;
+	unsigned int i;
 
-	p = malloc(nmemb * size);
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
 
+	p = malloc(nmemb * size);
 	if (p == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		p[i] = 0;
 	}
 	return (p);
 }
