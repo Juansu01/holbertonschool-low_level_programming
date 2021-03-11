0022859273ba8f27065027fbbd2663cc4f5a7566
Juansu01 <thejuank0@hotmail.com>
2021-03-11 00:55:55 +0000

    Added a new function that iterates an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - iterates an array and applies action to it.
+ *
+ * @size: this gets size_t.
+ *
+ * @array: this is the array we're using.
+ *
+ * @action: pointer to function
+ *
+ * Return: doesn't return.
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t a;
+	if (size && action)
+	{
+		for (a = 0; a < size; a++)
+		{
+			action(array[a]);
+		}
+	}
+}
