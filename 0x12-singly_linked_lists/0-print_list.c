9a507787404f02559b1090910480f6409278c199
Juansu01 <thejuank0@hotmail.com>
2021-03-19 18:50:49 +0000

    Added a funcion that prints nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * print_list - print elements of lists.
+ *
+ * @h: string.
+ *
+ * Return: Number of nodes.
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count;
+
+	count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nill)\n");
+			count++;
+			h = h->next;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+				count++;
+				h = h->next;
+		}
+	}
+	return(count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Here you will find the answers to the 0x12-singly_linked_lists tasks.
