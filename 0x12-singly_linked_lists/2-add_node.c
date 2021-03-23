cd8a2e64c744398057ce7151b87058c54d9590a7
Juansu01 <thejuank0@hotmail.com>
2021-03-23 16:00:25 +0000

    Added prototype and function that creates a node at beggining of list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * add_node - adds a node to the beggining of the list.
+ * @head: first node.
+ * @str: string with argument.
+ * Return: Address of new element.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+	int i;
+
+
+		newnode = malloc(sizeof(list_t));
+
+		if (newnode == NULL)
+		{
+			return (NULL);
+		}
+
+		while (str[i] != '\0')
+		{
+			i++;
+		}
+
+		newnode->str = strdup(str);
+		newnode->len = 1;
+		newnode->next = *head;
+
+		*head = newnode;
+
+		return (*head);
+}
