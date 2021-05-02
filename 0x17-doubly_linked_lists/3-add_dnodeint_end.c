705e957218f09512566f2b737a93ad9466b12a6b
Juansu01 <thejuank0@hotmail.com>
2021-05-02 13:37:29 -0500

    Modified code style for Betty

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,10 +1,16 @@
 #include "lists.h"
 
+/**
+* add_dnodeint_end - Function that adds node at the end of list.
+* @head: Pointer that points to first node.
+* @n: Content to be inserted into node.
+* Return: Returns pointer to newly allocated node.
+*/
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
-    dlistint_t *tmp_node;
+	dlistint_t *new_node;
+	dlistint_t *tmp_node;
 
 	if (head == NULL)
 	{
@@ -18,27 +24,27 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (NULL);
 	}
 
-    new_node->n = n;
+	new_node->n = n;
 
-    if (*head == NULL)
-    {
-        new_node->next = NULL;
-        new_node->prev = NULL;
-        *head = new_node;
-    }
-    else
-    {
-        tmp_node = *head;
+	if (*head == NULL)
+	{
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+	else
+	{
+		tmp_node = *head;
 
-        while (tmp_node->next != NULL)
-        {
-            tmp_node = tmp_node->next;
-        }
-        tmp_node->next = new_node;
-        new_node->prev = tmp_node;
-        new_node->next = NULL;
-    }
+		while (tmp_node->next != NULL)
+		{
+			tmp_node = tmp_node->next;
+		}
+		tmp_node->next = new_node;
+		new_node->prev = tmp_node;
+		new_node->next = NULL;
+	}
 
-	
+
 	return (new_node);
 }
