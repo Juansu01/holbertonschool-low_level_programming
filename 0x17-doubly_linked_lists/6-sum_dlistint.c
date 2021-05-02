384fa76ef720aa5d1c1b8ab4b4e3ad36638baf15
Juansu01 <thejuank0@hotmail.com>
2021-05-02 17:33:17 -0500

    Changed code style for Betty

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -9,21 +9,21 @@
 
 int sum_dlistint(dlistint_t *head)
 {
-    int sum = 0;
-    dlistint_t *tmp_node;
+	int sum = 0;
+	dlistint_t *tmp_node;
 
-    if (head == NULL)
-    {
-        return (0);
-    }
+	if (head == NULL)
+	{
+		return (0);
+	}
 
-    tmp_node = head;
+	tmp_node = head;
 
-    while (tmp_node != NULL)
-    {
-        sum += tmp_node->n;
-        tmp_node = tmp_node->next;
-    }
+	while (tmp_node != NULL)
+	{
+		sum += tmp_node->n;
+		tmp_node = tmp_node->next;
+	}
 
-    return (sum);
+	return (sum);
 }
