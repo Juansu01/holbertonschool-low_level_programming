c8c6a7cdc6791fdac37fec45df0420cddacd22ae
Juansu01 <thejuank0@hotmail.com>
2021-05-02 17:32:09 -0500

    Added function that sums content of list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+* sum_dlistint - Sums content of list.
+* @head: Pointer to first node.
+* Return: Returns sum.
+*
+*/
+
+int sum_dlistint(dlistint_t *head)
+{
+    int sum = 0;
+    dlistint_t *tmp_node;
+
+    if (head == NULL)
+    {
+        return (0);
+    }
+
+    tmp_node = head;
+
+    while (tmp_node != NULL)
+    {
+        sum += tmp_node->n;
+        tmp_node = tmp_node->next;
+    }
+
+    return (sum);
+}
