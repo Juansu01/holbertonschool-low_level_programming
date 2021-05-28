6f150e1ee7f78f77e82486051d53328a66f20b21
Juansu01 <thejuank0@hotmail.com>
2021-05-28 16:11:20 -0500

    Fixed betty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,30 +1,36 @@
 #include "hash_tables.h"
 
+/**
+* hash_table_print - Function that prints a hash table
+*
+* @ht: Hashtable.
+* Return: Doesn't return.
+*/
 
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int index;
-    int flag = 0;
+	unsigned long int index;
+	int flag = 0;
 
-    if (ht == NULL)
-    {
-        return;
-    }
+	if (ht == NULL)
+	{
+		return;
+	}
 
-    printf("{");
+	printf("{");
 
-    for (index = 0; index < ht->size; index++)
-    {
-        while (ht->array[index] != NULL)
-        {
-            if (flag == 1)
-            {
-                printf(", ");
-            }
-            printf("'%s': '%s'", ht->array[index]->key, ht->array[index]->value);
-            ht->array[index] = ht->array[index]->next;
-            flag = 1;
-        }
-    }
-    printf("}\n");
+	for (index = 0; index < ht->size; index++)
+	{
+		while (ht->array[index] != NULL)
+		{
+			if (flag == 1)
+			{
+				printf(", ");
+			}
+			printf("'%s': '%s'", ht->array[index]->key, ht->array[index]->value);
+			ht->array[index] = ht->array[index]->next;
+			flag = 1;
+		}
+	}
+	printf("}\n");
 }
