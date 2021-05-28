6641b7822fe7f9b45ca73daed8f63d831ba01066
Juansu01 <thejuank0@hotmail.com>
2021-05-28 14:51:53 -0500

    Fixed code style and bety

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,38 +1,42 @@
 #include "hash_tables.h"
 
 /**
+* hash_table_get - This function gets the element at a given key.
 *
+* @ht: Hash table.
+* @key: Key to look for.
+* Return: Pointer to value or NULL if it fails.
 */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    hash_node_t *tmp = NULL;
-    unsigned long int index = 0;
+	hash_node_t *tmp = NULL;
+	unsigned long int index = 0;
 
-    if (ht == NULL)
-    {
-        return (NULL);
-    }
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
 
-    if (key == NULL)
-    {
-        return (NULL);
-    }
+	if (key == NULL)
+	{
+		return (NULL);
+	}
 
-    index = key_index((unsigned const char *)key, ht->size);
-    tmp = ht->array[index];
+	index = key_index((unsigned const char *)key, ht->size);
+	tmp = ht->array[index];
 
-    while (tmp != NULL)
-    {
-        if (strcmp(key, tmp->key) == 0)
-        {
-            return (tmp->value);
-        }
-        else
-        {
-            tmp = tmp->next;
-        }
-    }
+	while (tmp != NULL)
+	{
+		if (strcmp(key, tmp->key) == 0)
+		{
+			return (tmp->value);
+		}
+		else
+		{
+			tmp = tmp->next;
+		}
+	}
 
-    return (NULL);
+	return (NULL);
 }
