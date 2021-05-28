eea30b3cadbc7a01298fee9d929cf97fc1aba433
Juansu01 <thejuank0@hotmail.com>
2021-05-28 10:20:27 -0500

    Added betty section

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,21 +1,26 @@
 #include "hash_tables.h"
-
+/**
+* hash_table_create - This function creates a new hash table.
+*
+* @size: This number determines the size of the hash table.
+* Return: Returns a pointer to the new hash table.
+*/
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table;
+	hash_table_t *hash_table;
 
-    if (size <= 0)
-    {
-        return (NULL);
-    }
+	if (size <= 0)
+	{
+		return (NULL);
+	}
 
-    hash_table = malloc(sizeof(hash_table_t) * size);
+	hash_table = malloc(sizeof(hash_table_t) * size);
 
-    if (hash_table == NULL)
-    {
-        return (NULL);
-    }
+	if (hash_table == NULL)
+	{
+		return (NULL);
+	}
 
-    return (hash_table);
+	return (hash_table);
 }
