c609fbb3895d6165ddc6e6bbda6e75e52c1446af
Juansu01 <thejuank0@hotmail.com>
2021-05-28 10:17:48 -0500

    Added negative or 0 size support

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,6 +5,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *hash_table;
 
+    if (size <= 0)
+    {
+        return (NULL);
+    }
+
     hash_table = malloc(sizeof(hash_table_t) * size);
 
     if (hash_table == NULL)
