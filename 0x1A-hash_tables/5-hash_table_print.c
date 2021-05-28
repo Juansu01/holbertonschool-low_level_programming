6bfde15cb552dca4a22bde233b867a4b7e16cea9
Juansu01 <thejuank0@hotmail.com>
2021-05-28 16:09:23 -0500

    Added function that prints a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned long int index;
+    int flag = 0;
+
+    if (ht == NULL)
+    {
+        return;
+    }
+
+    printf("{");
+
+    for (index = 0; index < ht->size; index++)
+    {
+        while (ht->array[index] != NULL)
+        {
+            if (flag == 1)
+            {
+                printf(", ");
+            }
+            printf("'%s': '%s'", ht->array[index]->key, ht->array[index]->value);
+            ht->array[index] = ht->array[index]->next;
+            flag = 1;
+        }
+    }
+    printf("}\n");
+}
