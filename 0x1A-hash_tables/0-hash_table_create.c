a7f04de24c0ed1af341d3463f6bc28c17b29ef25
Juansu01 <thejuank0@hotmail.com>
2021-05-28 10:39:21 -0500

    Allocated memory for array and initialized it to NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,18 +8,32 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
+	hash_table_t *hash_table = NULL;
+	unsigned long int iter;
 
 	if (size <= 0)
 	{
 		return (NULL);
 	}
 
-	hash_table = malloc(sizeof(hash_table_t) * size);
+	hash_table = malloc(sizeof(hash_table_t));
 
 	if (hash_table == NULL)
 	{
 		return (NULL);
+	}
+
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t) * size);
+	
+	if (hash_table->array == NULL)
+	{
+		return (NULL);
+	}
+
+	for(iter = 0; iter < size; iter++)
+	{
+		hash_table->array[iter] = NULL;
 	}
 
 	return (hash_table);
